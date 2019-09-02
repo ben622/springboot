87cb4ae5d13f4be802ed924b6bcab73affd9e2f7
ben622 <zhangchuan622@gmail.com>
2019-09-02 16:07:27 +0800

    rtmp流权限控制

--- src/main/java/com/ben/java/springboot/controller/AuthorizeController.java
+++ src/main/java/com/ben/java/springboot/controller/AuthorizeController.java
@@ -8,11 +8,15 @@ import com.ben.java.springboot.repository.AuthorizeRepository;
 import com.ben.java.springboot.util.ResultCode;
 import com.ben.java.springboot.util.ResultFactory;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 授权管理
@@ -20,29 +24,40 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/chat/auth")
 public class AuthorizeController {
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private AuthorizeRepository repository;
 
     /**
      * 验证客户端的accid是否合法
+     * <p>
+     * HTTP 2xx代码继续RTMP会话
+     * HTTP 3xx将RTMP重定向到另一个流，其名称取自 LocationHTTP响应头。如果启动了新的流名称，rtmp:// 则会创建远程中继。中继要求指定IP地址而不是域名，并且仅适用于大于1.3.10的nginx版本。另见notify_relay_redirect。
+     * 其他将丢弃RTMP连接
      *
      * @param accid
      * @return
      */
-    @ResponseBody
     @RequestMapping("/verifyAccid")
-    public Result<ChatroomInfo> createChatroom(@Param("accid") String accid) throws ApiException {
+    public void verifyRTMP(HttpServletResponse response, @Param("accid") String accid) throws Exception {
         if (StringUtils.isBlank(accid)) {
-            throw new ApiException(ResultCode.ACCID_VERIFY_FAILURE, "accid verify failure，please check your accid");
+            logger.info("accid verify failure，please check your accid");
+            response.setStatus(500);
+            response.getWriter().append("accid verify failure，please check your accid");
+            return;
         }
         AuthorizeInfo authorizeInfo = repository.findAuthorizeInfoByAccid(accid);
-
         if (authorizeInfo == null) {
-            return ResultFactory.obtainResult(500, "accid verify failure，please check your accid", 0, accid);
+            logger.info("accid verify failure，please check your accid");
+            response.setStatus(500);
+            response.getWriter().append("accid verify failure，please check your accid");
+            return;
         }
 
-        return ResultFactory.obtainResult(200, "accid verify successful", 1, accid);
+
+        logger.info("accid verify successful");
+        response.setStatus(200);
+        response.getWriter().append("accid verify successful");
     }
 
 
